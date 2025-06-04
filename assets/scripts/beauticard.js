086752405f61d0ce0c6b63f6dae7241f0caea9aa
dibesfer <dibesfer@gmail.com>
2025-06-04 17:23:34 +0200

    yaya

--- assets/scripts/beauticard.js
+++ assets/scripts/beauticard.js
@@ -6,11 +6,61 @@ fetch("/assets/json/beauticard.json")
    
     elements.forEach(element => {
       const card = document.createElement("div");
-      card.classList.add("card");
+      card.classList.add("beauticard");
+
+
+
+
+      
+
+
+
+
       card.innerHTML = `
-        <h2>${element.name}</h2>
-        <p>Tipo: ${element.description}</p>
-        <p>Nivel: ${element.nivel}</p>
+        <div class="bc-wrapper">
+            <div class="beauticard-title">
+                <div id="bt-1">1</div>
+                <div id="bt-2"><b>${element.name}</b></div>
+                <div id="bt-3">
+                    <img style="vertical-align: middle;"
+                        src="https://upload.wikimedia.org/wikipedia/commons/f/f7/Japanese_Imperial_Seal.svg"
+                        alt="">
+                </div>
+
+                <!-- 1. "Exodia" (エクゾディア, Ekuzodia)  -->
+            </div>
+
+            <div class="beauticard-stars">
+                <img src="https://static.vecteezy.com/system/resources/previews/022/133/469/non_2x/star-shape-star-icon-yellow-star-in-rating-symbol-free-png.png"
+                    alt="">
+                <img src="https://static.vecteezy.com/system/resources/previews/022/133/469/non_2x/star-shape-star-icon-yellow-star-in-rating-symbol-free-png.png"
+                    alt="">
+                <img src="https://static.vecteezy.com/system/resources/previews/022/133/469/non_2x/star-shape-star-icon-yellow-star-in-rating-symbol-free-png.png"
+                    alt="">
+                <img src="https://static.vecteezy.com/system/resources/previews/022/133/469/non_2x/star-shape-star-icon-yellow-star-in-rating-symbol-free-png.png"
+                    alt="">
+                <img src="https://static.vecteezy.com/system/resources/previews/022/133/469/non_2x/star-shape-star-icon-yellow-star-in-rating-symbol-free-png.png"
+                    alt="">
+            </div>
+
+            <div class="beauticard-image">
+                <img src="${element.image}"
+                    alt="">
+            </div>
+
+            <div class="beauticard-description">
+                <h3>[${element.type}]</h3>
+                <p>${element.desc}</p>
+                <div id="bt-d-under">
+                    <hr style="border: solid 1px black; margin: 0;">
+                    <p style="text-align: right;">ATK/3000 DEF/2500 </p>
+                </div>
+
+            </div>
+            <div class="beauticard-extra">
+                9196 1ª Edition &copy; Ferrán Minero 1996
+            </div>
+        </div>
       `;
       contenedor.appendChild(card);
     });
